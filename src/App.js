cf02899344a32619c0568c98e882439d4d51111e
Julien Daenen <daenen.julien.pro@gmail.com>
2021-10-18 16:06:37 +0200

    add timer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import React from "react";
+import Count from "./components/Count";
 
 export default function App(){
-    return <h1>Hello World!</h1>
+
+    const [seconds, setSeconds] = React.useState(10);
+
+    return (
+        <>
+            <h1>Pomodoro</h1>
+            <Count className="timer" seconds={seconds} setSeconds={setSeconds}/>
+            <button className="add">+</button>
+            <button className="substr">-</button>
+            <button className="start">start</button>
+            <button className="reset">reset</button>
+        </>
+    );
 }
---
+++ src/components/Count.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+export default function Count({ seconds, setSeconds }){
+    React.useEffect(() => {
+        if (seconds > 0) {
+            setTimeout(() => setSeconds(seconds - 1), 1000);
+        }
+    });
+
+    let m = Math.floor(seconds % 3600 / 60);//minutes
+    let s = Math.floor(seconds % 3600 % 60);//seconds
+    if(seconds > 0){
+        if(m > 9 && s > 9){
+            return(
+                <div>{m}:{s}</div>
+            )
+        }else if(m >= 0 && m <= 9 && s > 9){
+            return(
+                <div>0{m}:{s}</div>
+            )
+        }else if(m > 9 && s >= 0 && s <= 9){
+            return(
+                <div>{m}:0{s}</div>
+            )
+        }else if(m >= 0 && m <= 9 && s >= 0 && s <= 9){
+            return(
+                <div>0{m}:0{s}</div>
+            )
+        }
+        return(
+            <div>{m}:{s}</div>
+        )
+    }else{
+        return(
+            <div>00:00</div>
+        )
+    }
+}
